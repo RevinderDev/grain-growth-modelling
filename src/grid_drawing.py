4bfd5d18c2514152a35cc90f156d4ce7529dbfce
MichalKasprzyk <michal0kasprzyk@gmail.com>
2018-11-28 00:33:30 +0100

    xx

--- src/grid_drawing.py
+++ src/grid_drawing.py
@@ -8,29 +8,32 @@
 
  Explanation video: http://youtu.be/mdTeqiWyFnc
 """
+import random
+
 import pygame
+
+from src.color import RandomColorDictionary
+
 
 class Grid:
 
     def __init__(self):
-        # Define some colors
-        self.BLACK = (0, 0, 0)
-        self.WHITE = (255, 255, 255)
-        self.GREEN = (0, 255, 0)
-        self.RED = (255, 0, 0)
 
-        # Define grid sizes
-        self.GRID_SIZE_X = 20
-        self.GRID_SIZE_Y = 20
+        self.neighbourhood_type = 'Moore'
+        self.grain_growth = False
 
         # This sets the WIDTH and HEIGHT of each grid location
-        self.WIDTH = 5
-        self.HEIGHT = 5
+        self.WIDTH = 7
+        self.HEIGHT = 7
 
         # This sets the margin between each cell
         self.MARGIN = 1
 
-    def init_grid(self):
+    def init_grid(self, X, Y):
+        # Define grid sizes
+        self.GRID_SIZE_X = X
+        self.GRID_SIZE_Y = Y
+
         # Create a 2 dimensional array. A two dimensional
         # array is simply a list of lists.
         self.grid = []
@@ -43,8 +46,19 @@ class Grid:
 
         # Set row 1, cell 5 to one. (Remember rows and
         # column numbers start at zero.)
-        self.grid[1][5] = 1
-        self.grid[19][19] = 1
+        # self.grid[1][5] = 1
+        #self.grid[19][19] = 1
+
+    def clean_grid(self):
+        for row in range(self.GRID_SIZE_X):
+            for column in range(self.GRID_SIZE_Y):
+                self.grid[row][column] = 0
+
+    def change_neighbour(self, neighbour):
+        self.neighbourhood_type = neighbour
+
+    def set_grain_growth(self, grain_growth_bool):
+        self.grain_growth = grain_growth_bool
 
 
 class PyGameWindow:
@@ -54,20 +68,63 @@ class PyGameWindow:
         pygame.init()
         # Set the HEIGHT and WIDTH of the screen
         # WINDOW_SIZE = [600, 600]
-        self.WINDOW_SIZE = [Grid.GRID_SIZE_Y * (Grid.HEIGHT + Grid.MARGIN) + Grid.MARGIN, Grid.GRID_SIZE_X * (Grid.WIDTH + Grid.MARGIN) + Grid.MARGIN]
+        self.WINDOW_SIZE = [Grid.GRID_SIZE_Y * (Grid.HEIGHT + Grid.MARGIN) + Grid.MARGIN,
+                            Grid.GRID_SIZE_X * (Grid.WIDTH + Grid.MARGIN) + Grid.MARGIN]
         self.screen = pygame.display.set_mode(self.WINDOW_SIZE)
 
         # Set title of screen
-        pygame.display.set_caption("Array Backed Grid")
+        pygame.display.set_caption("Grain Growth")
 
         # Loop until the user clicks the close button.
         self.done = False
+        self.color_class = RandomColorDictionary()
 
         # Used to manage how fast the screen updates
         self.clock = pygame.time.Clock()
         self.main_loop(Grid)
 
-    def main_loop(self,Grid):
+    def moore_growth(self, Grid, row, column):
+        grain_to_grow = []
+        color_to_grow = Grid.grid[row][column]
+        grain_to_grow.append(color_to_grow)
+
+        if column + 1 < Grid.GRID_SIZE_Y:
+            grain_to_grow.append(row)
+            grain_to_grow.append(column + 1)
+
+        if row - 1 >= 0 and column + 1 < Grid.GRID_SIZE_Y:
+            grain_to_grow.append(row - 1)
+            grain_to_grow.append(column + 1)
+
+        if row - 1 >= 0:
+            grain_to_grow.append(row - 1)
+            grain_to_grow.append(column)
+
+        if row - 1 >= 0 and column - 1 >= 0:
+            grain_to_grow.append(row - 1)
+            grain_to_grow.append(column - 1)
+
+        if column - 1 >= 0:
+            grain_to_grow.append(row)
+            grain_to_grow.append(column - 1)
+
+        if row + 1 < Grid.GRID_SIZE_X and column - 1 >= 0:
+            grain_to_grow.append(row + 1)
+            grain_to_grow.append(column - 1)
+
+        if row + 1 < Grid.GRID_SIZE_X:
+            grain_to_grow.append(row + 1)
+            grain_to_grow.append(column)
+
+        if row + 1 < Grid.GRID_SIZE_X and column + 1 < Grid.GRID_SIZE_X:
+            grain_to_grow.append(row + 1)
+            grain_to_grow.append(column + 1)
+
+
+        return grain_to_grow
+
+
+    def main_loop(self, Grid):
         # -------- Main Program Loop -----------
         while not self.done:
             for event in pygame.event.get():  # User did something
@@ -80,24 +137,41 @@ class PyGameWindow:
                     column = pos[0] // (Grid.WIDTH + Grid.MARGIN)
                     row = pos[1] // (Grid.HEIGHT + Grid.MARGIN)
                     # Set that location to one
-                    Grid.grid[row][column] = 1
+                    Grid.grid[row][column] = random.choice(list(self.color_class.colors))
+                    # TEST Methods of click in here
                     print("Click ", pos, "Grid coordinates: ", row, column)
 
             # Set the screen background
-            self.screen.fill(Grid.BLACK)
+            self.screen.fill(self.color_class.colors['black'])
 
             # Draw the grid
+            grains_to_grow = []
             for row in range(Grid.GRID_SIZE_X):
                 for column in range(Grid.GRID_SIZE_Y):
-                    color = Grid.WHITE
-                    if Grid.grid[row][column] == 1:
-                        color = Grid.GREEN
+                    color = self.color_class.colors['white']
+                    if Grid.grid[row][column] != 0:
+                        if Grid.grain_growth == True:
+                            # MOORE test
+                            if Grid.neighbourhood_type == 'Moore':
+                                grain_to_grow = self.moore_growth(Grid, row, column)
+                                grains_to_grow.append(grain_to_grow)
+                                grain_to_grow = []
+
+                        color = self.color_class.colors[Grid.grid[row][column]]
                     pygame.draw.rect(self.screen,
                                      color,
                                      [(Grid.MARGIN + Grid.WIDTH) * column + Grid.MARGIN,
                                       (Grid.MARGIN + Grid.HEIGHT) * row + Grid.MARGIN,
                                       Grid.WIDTH,
                                       Grid.HEIGHT])
+
+            if Grid.grain_growth == True:
+                for i in range(0, len(grains_to_grow)):
+                    for j in range(1, len(grains_to_grow[i]), 2):
+                        row = grains_to_grow[i][j]
+                        column = grains_to_grow[i][j+1]
+                        Grid.grid[row][column] = grains_to_grow[i][0]
+            grains_to_grow = []
 
             # Limit to 60 frames per second
             self.clock.tick(60)
@@ -108,5 +182,4 @@ class PyGameWindow:
         # Be IDLE friendly. If you forget this line, the program will 'hang'
         # on exit.
         pygame.quit()
-
-
+        exit()
--- src/main.py
+++ src/main.py
@@ -1,24 +1,199 @@
-# import the library
-from appJar import gui
-
-# handle button events
+import wx
+import threading
 from src.grid_drawing import Grid, PyGameWindow
 
 
-def press(button):
-    if button == "Exit":
-        app.stop()
-    elif button == "Start":
-        grid = Grid()
-        pyWindow = PyGameWindow()
+class Frame(wx.Frame):
 
-# create a GUI variable called app
-app = gui("Login Window", "400x200")
-app.setFont(18)
+    def init_ui(self):
+        # --- Main sizers ---
+        self.sizer_ver_main = wx.BoxSizer(wx.VERTICAL)
+        self.sizer_ver_input = wx.BoxSizer(wx.VERTICAL)
+        self.font_size = 9
 
-# link the buttons to the function called press
-app.addButtons(["Start","Exit"], press)
+        # --- Init UI functions ---
+        self.init_helpers()
+        self.init_grid_size()
+        self.init_neigh_combo_box()
+        self.init_create_grid_button()
 
 
-# start the GUI
-app.go()
+
+        self.init_control_buttons()
+
+        # --- Set main sizers ---
+        self.sizer_ver_main.Add(self.sizer_ver_input, 0, wx.EXPAND, 5)
+        self.SetSizer(self.sizer_ver_main)
+        self.Layout()
+        self.Centre(wx.BOTH)
+        # and a status bar
+        self.CreateStatusBar()
+        self.SetStatusText("Welcome to student project of grain growth!")
+
+    def init_control_buttons(self):
+        sizer_ver_control_buttons = wx.BoxSizer(wx.HORIZONTAL)
+
+        # --- Start button ---
+        self.start_drawing_grid_button = wx.Button(self, wx.ID_ANY, u"Start", wx.DefaultPosition,
+                                                   wx.DefaultSize, 0)
+        self.start_drawing_grid_button.Bind(wx.EVT_BUTTON, self.on_start)
+        sizer_ver_control_buttons.Add(self.start_drawing_grid_button, 5, wx.EXPAND, 5)
+
+        # --- Pause button ---
+        self.pause_drawing_grid_button = wx.Button(self, wx.ID_ANY, u"Pause", wx.DefaultPosition,
+                                                   wx.DefaultSize, 0)
+        self.pause_drawing_grid_button .Bind(wx.EVT_BUTTON, self.on_pause)
+        sizer_ver_control_buttons.Add(self.pause_drawing_grid_button , 5, wx.EXPAND, 5)
+
+        # --- Clean button ---
+        self.clean_grid_button = wx.Button(self, wx.ID_ANY, u"Clean", wx.DefaultPosition,
+                                                   wx.DefaultSize, 0)
+        self.clean_grid_button .Bind(wx.EVT_BUTTON, self.on_clean)
+        sizer_ver_control_buttons.Add(self.clean_grid_button , 5, wx.EXPAND, 5)
+
+        self.sizer_ver_input.Add(sizer_ver_control_buttons, 0, wx.EXPAND, 5)
+
+
+    def init_neigh_combo_box(self):
+        sizer_hor_neigh = wx.BoxSizer(wx.HORIZONTAL)
+
+        # --- Label neigh choice ----
+        self.label_neigh_choice = wx.StaticText(self, wx.ID_ANY, u"Choose neighbourhood:", wx.DefaultPosition,
+                                                wx.DefaultSize, 0)
+        self.label_neigh_choice.SetFont(wx.Font(wx.FontInfo(self.font_size)))
+        self.label_neigh_choice.Wrap(-1)
+        # --- Neighboor combo box ---
+        self.neigh_choices_array = ['Moore', 'Von Neumann', 'Hexagonal Left', 'Hexagonal Right', 'Random Hexagonal',
+                                    'Random Pentagonal']
+        self.neigh_combo = wx.ComboBox(self, wx.ID_ANY, "Moore", choices=self.neigh_choices_array)
+        self.neigh_combo.Bind(wx.EVT_COMBOBOX, self.change_neighbourhood)
+
+        sizer_hor_neigh.Add(self.label_neigh_choice, 0, wx.ALL, 5)
+        sizer_hor_neigh.Add(self.neigh_combo, 0, wx.ALL, 5)
+
+        self.sizer_ver_input.Add(sizer_hor_neigh, 0, wx.ALL, 5)
+
+    def init_helpers(self):
+        self.empty_label = wx.StaticText(self, wx.ID_ANY, u"", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.empty_label.Wrap(-1)
+
+    def init_grid_size(self):
+        sizer_hor_grid_size = wx.BoxSizer(wx.HORIZONTAL)
+
+        # --- Label grid size ---
+        sizer_ver_grid_size_labels = wx.BoxSizer(wx.VERTICAL)
+
+        self.label_grid_size = wx.StaticText(self, wx.ID_ANY, u"Grid size:", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.label_grid_size.SetFont(wx.Font(wx.FontInfo(self.font_size)))
+        self.label_grid_size.Wrap(-1)
+
+        sizer_ver_grid_size_labels.Add(self.empty_label, 0, wx.ALL, 5)
+        sizer_ver_grid_size_labels.Add(self.label_grid_size, 0, wx.ALL, 5)
+        sizer_hor_grid_size.Add(sizer_ver_grid_size_labels, 0, wx.ALL, 5)
+
+        # --- Input grid size X ---
+        sizer_ver_grid_size_input_x = wx.BoxSizer(wx.VERTICAL)
+
+        self.label_grid_size_x = wx.StaticText(self, wx.ID_ANY, u"Width:")
+        self.label_grid_size_x.Wrap(-1)
+        self.label_grid_size_x.SetFont(wx.Font(wx.FontInfo(self.font_size)))
+        self.input_grid_size_x = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        self.input_grid_size_x.SetValue("100")
+
+        sizer_ver_grid_size_input_x.Add(self.label_grid_size_x, 0, wx.ALL, 5)
+        sizer_ver_grid_size_input_x.Add(self.input_grid_size_x, 0, wx.ALL, 5)
+        sizer_hor_grid_size.Add(sizer_ver_grid_size_input_x, 0, wx.ALL, 5)
+
+        # --- Input grid size Y ---
+        sizer_ver_grid_size_input_y = wx.BoxSizer(wx.VERTICAL)
+
+        self.label_grid_size_y = wx.StaticText(self, wx.ID_ANY, u"Height:")
+        self.label_grid_size_y.Wrap(-1)
+        self.label_grid_size_y.SetFont(wx.Font(wx.FontInfo(self.font_size)))
+        self.input_grid_size_y = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        self.input_grid_size_y.SetValue("100")
+
+        sizer_ver_grid_size_input_y.Add(self.label_grid_size_y, 0, wx.ALL, 5)
+        sizer_ver_grid_size_input_y.Add(self.input_grid_size_y, 0, wx.ALL, 5)
+        sizer_hor_grid_size.Add(sizer_ver_grid_size_input_y, 0, wx.ALL, 5)
+
+        self.sizer_ver_input.Add(sizer_hor_grid_size, 0, wx.ALL, 5)
+
+    def init_create_grid_button(self):
+        # --- Start drawing button ---
+        self.create_drawing_grid_button = wx.Button(self, wx.ID_ANY, u"Create drawing grid", wx.DefaultPosition,
+                                                   wx.DefaultSize, 0)
+        self.create_drawing_grid_button .Bind(wx.EVT_BUTTON, self.create_grid)
+
+        self.sizer_ver_input.Add(self.create_drawing_grid_button , 0, wx.EXPAND, 5)
+
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title="Grain Growth", pos=wx.DefaultPosition,
+                          size=wx.Size(370, 600), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+        self.is_thread_alive = False
+        self.init_ui()
+
+
+    def on_pause(self,event):
+        self.drawing_thread.grid.grain_growth = False
+
+    def on_start(self, event):
+        self.drawing_thread.neigh_choice = self.neigh_combo.GetValue()
+        self.drawing_thread.grid.grain_growth = True
+
+    def on_clean(self,event):
+        self.drawing_thread.grid.clean_grid()
+
+    def change_neighbourhood(self, event):
+        print("Chosen neighbourhood: " + self.neigh_combo.GetValue())
+        self.drawing_thread.neigh_choice = self.neigh_combo.GetValue()
+
+    def create_grid(self, event):
+        x_coordinate = int(self.input_grid_size_x.GetValue())
+        y_coordinate = int(self.input_grid_size_y.GetValue())
+        self.drawing_thread = DrawingThread()
+        self.drawing_thread.set_coords(x_coordinate, y_coordinate)
+        self.drawing_thread.start()
+
+    def OnExit(self, event):
+        """Close the frame, terminating the application."""
+        self.drawing_thread.join()
+        self.Close(True)
+
+
+class DrawingThread(threading.Thread):
+
+    def __init__(self):
+        super(DrawingThread, self).__init__()
+        self.x = 100
+        self.y = 100
+        self.grid = Grid()
+
+    def run(self):
+        self.grid.init_grid(self.x, self.y)
+        self.grid_window = PyGameWindow(self.grid)
+        self.grid_window.main_loop(self.grid)
+
+    @property
+    def neigh_choice(self):
+        return self.grid.neighbourhood_type
+
+    @neigh_choice.setter
+    def neigh_choice(self, value):
+        self.grid.neighbourhood_type = value
+
+
+    def set_coords(self, x, y):
+        if x <= 0 or y <= 0:
+            raise ValueError("Coords cannot be negative!")
+        self.x = x
+        self.y = y
+
+
+if __name__ == '__main__':
+    # When this module is run (not imported) then create the app, the
+    # frame, show it, and start the event loop.
+    app = wx.App()
+    frm = Frame(None)
+    frm.Show()
+    app.MainLoop()
